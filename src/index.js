6b066136c64f67ac304e325103c96f2a50b4795f
avisas <alejandra.visa91@gmail.com>
2019-11-03 17:22:07 -0500

    firebase/app in index.js added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from "firebase";
+import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
