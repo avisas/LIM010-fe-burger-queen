4740939751d58184b1cd74406a12c479e2302878
avisas <alejandra.visa91@gmail.com>
2019-11-03 17:19:17 -0500

    firebase added

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../img/logo.png';
 import './login.css';
+import { signIn } from '../../index.js';
 
-function Login() {
-  return (
-    <div className="Login-user">
-      <div className="Login-header">
-        <img src={logo} className="Login-logo" alt="logo loginUser" />
-        <p>Burger Queen</p>
-      </div>
-      <div className="Login-body">
-        Este es el login
-        <form className="login-form">
+class Login extends Component {
+  state = {
+    user: '',
+    password: '',
+  }
 
-        </form>
+  handleSubmit = (event) => {
+    event.preventDefault();
+    signIn(this.state.user, this.state.password)
+      .then(() => {
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+        alert(err.message);
+      })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="Login-user">
+        <div className="Login-header">
+          <p>Burger Queen</p>
+        </div>
+        <div className="Login-body">
+          <img className="Login-logo" src={logo} alt="logo loginUser" />
+          <form className="login-form" onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="username" id="user" onChange={this.handleChange} />
+            <input type="password" placeholder="password" id="password" onChange={this.handleChange} />
+            <button>login</button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
+};
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAz0pBBORl0oSm5EtfUuMffxwn51ccPCvs",
+  authDomain: "burgerqueen-e600d.firebaseapp.com",
+  databaseURL: "https://burgerqueen-e600d.firebaseio.com",
+  projectId: "burgerqueen-e600d",
+  storageBucket: "burgerqueen-e600d.appspot.com",
+  messagingSenderId: "415155745303",
+  appId: "1:415155745303:web:36a760855ecc868584e252"
+};
+
+firebase.initializeApp(firebaseConfig);
+const signIn = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
+const signOut =() => firebase.auth().signOut();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+export { signIn, signOut };
